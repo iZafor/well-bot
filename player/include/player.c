19a8f8f4b5e8146d620a8c829630d6d9d52644d1
iZafor <izafor65@gmail.com>
2024-08-26 20:21:33 +0600

    add function definition for player_read_file

--- player/include/player.c
+++ player/include/player.c
@@ -34,3 +34,30 @@ int play(char *filename) {
         player_quit(&state);
         return errcode;
 }
+
+int player_read_file(PlayerState *state) {
+    int errcode = 0;
+
+    if ((errcode = avformat_open_input(&state->avfc, state->filename, NULL, NULL)) < 0) {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION,
+            "Failed to open input file! Message: %s\n",
+            av_err2str(errcode)
+        );
+        return errcode;
+    }
+    SDL_Log("File opened\n");
+
+    if ((errcode = avformat_find_stream_info(state->avfc, NULL)) < 0) {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION,
+            "Failed to read stream info! Message: %s\n",
+            av_err2str(errcode)
+        );
+        return errcode;
+    }
+    SDL_Log("Read stream info\n");
+    av_dump_format(state->avfc, 0, state->filename, 0);
+
+    return errcode;
+}
